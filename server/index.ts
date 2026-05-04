50c23750f526b090e8a47991273b3e0aaa4e64fa
Miguel Nicolas <miguelqnicolas@gmail.com>
2026-05-04 00:46:27 -0400

    Increase body-parser limit

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const port = Number(process.env.PORT ?? 3001);
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "15mb" }));
 
 app.get("/", (_request, response) => {
   response.json({ ok: true });
